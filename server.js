92fa387b52afaa614a22b57dbf79449d7f86caa3
Ella Horn <ellalouisahorn@yahoo.com>
2020-01-08 19:50:17 -0500

    set up movie api

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const express = require("express");
+const morgan = require("morgan");
+const movies = require("./moviedex-api");
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/movies", (req, res) => {
+  res.json(movies);
+});
+
+app.listen(7000, () => {
+  console.log("server started on PORT 7000");
+});
