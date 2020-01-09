dfa3e8387719c8a996a52e5d2bcd9f089b17aa1d
Ella Horn <ellalouisahorn@yahoo.com>
2020-01-09 17:38:59 -0500

    shtuff

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const movies = require("./moviedex-api");
-
-console.log(process.env.API_TOKEN);
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIES = require("./moviedex-api");
 
 const app = express();
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
 
-app.get("/movie", (req, res) => {
-  console.log("hello");
-  const { genre = "", country = "", avg_vote = "" } = req.query;
-  let results1 = movies.filter(movie =>
-    movie.genre.toLowerCase().includes(genre.toLowerCase())
-  );
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  console.log(apiToken);
+  const authToken = req.get("Authorization");
+  console.log(authToken);
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+app.get("/movie", function getMovie(req, res) {
+  let response = MOVIES;
+  const { genre, country, avg_vote } = req.query;
+  if (genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+  if (country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+  if (avg_vote) {
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(avg_vote)
+    );
+  }
+  res.json(response);
 });
 
 const PORT = 7000;
