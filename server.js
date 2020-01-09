e44f52ebd63efc50333e43c9d3997cf913c46786
Ella Horn <ellalouisahorn@yahoo.com>
2020-01-08 20:09:32 -0500

    i dont know what any of this is

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const movies = require("./moviedex-api");
+
+console.log(process.env.API_TOKEN);
 
 const app = express();
 app.use(morgan("dev"));
 
 app.get("/movie", (req, res) => {
-  res.json(movies);
+  console.log("hello");
+  const { genre = "", country = "", avg_vote = "" } = req.query;
+  let results1 = movies.filter(movie =>
+    movie.genre.toLowerCase().includes(genre.toLowerCase())
+  );
 });
 
-app.listen(7000, () => {
-  console.log("server started on PORT 7000");
+const PORT = 7000;
+
+app.listen(PORT, () => {
+  console.log(`server started on PORT ${PORT}`);
 });
